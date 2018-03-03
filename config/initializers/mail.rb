8c6b4928909e0cf445ccd9b36cb862382ad6f5f8
Richard Travis <r.z.travis@gmail.com>
2018-03-03 13:27:00 -0700

    Further mail configuration

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -11,20 +11,3 @@ ActionMailer::Base.delivery_method = :smtp
 
 # Makes debugging *way* easier.
 ActionMailer::Base.raise_delivery_errors = true
-
-# This interceptor just makes sure that local mail
-# only emails you.
-# http://edgeguides.rubyonrails.org/action_mailer_basics.html#intercepting-emails
-class DevelopmentMailInterceptor
-  def self.delivering_email(message)
-    message.to =  'r.z.travis@gmail.com'
-    message.cc = nil
-    message.bcc = nil
-  end
-end
-
-# Locally, outgoing mail will be 'intercepted' by the
-# above DevelopmentMailInterceptor before going out
-if Rails.env.development?
-  ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
-end
