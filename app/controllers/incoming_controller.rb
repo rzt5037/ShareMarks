f7762d1ea81d357c3f1ad5dc8b29714c614213c9
Richard Travis <r.z.travis@gmail.com>
2018-03-04 19:43:43 -0700

    Further updates

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -10,7 +10,7 @@ class IncomingController < ApplicationController
     # Find the user by using params[:sender]
       @user = User.find_by(:email => params[:sender])
     # Find the topic by using params[:subject]
-      @topic = Topic.find_by(:title => params[:subject.chomp])
+      @topic = Topic.find_by(:title => params[:subject])
     # Assign the url to a variable after retreiving it from params["body-plain"]
       @url = params["body-plain".chomp]
     # Check if user is nil, if so, create and save a new user
@@ -19,7 +19,7 @@ class IncomingController < ApplicationController
       end
     # Check if the topic is nil, if so, create and save a new topic
       unless @topic
-        @topic = Topic.create!(:title => params[:subject.chomp], :user_id => @user.id)
+        @topic = Topic.create!(:title => params[:subject], :user_id => @user.id)
       end
     # Now that you're sure you have a valid user and topic, build and save a new bookmark
       Bookmark.create!(:url => @url, :topic_id => @topic.id)
